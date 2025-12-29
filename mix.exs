8129a189e82547f0a8ae592a01c7ea814697127f
Andrei Zvonimir Crnković <andreicek@0x7f.dev>
2025-12-29 19:36:26 +0100

    Fixes to xml

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCius.MixProject do
   def project do
     [
       app: :ex_cius,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       description: description(),
