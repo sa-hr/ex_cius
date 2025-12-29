584154bf191c5c2bcf4e148b8321a0301cdf6ff4
Andrei Zvonimir Crnković <andreicek@0x7f.dev>
2025-12-29 17:24:53 +0100

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCius.MixProject do
   def project do
     [
       app: :ex_cius,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       description: description(),
