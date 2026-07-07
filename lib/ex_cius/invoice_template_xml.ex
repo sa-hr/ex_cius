c331f3837ae3cf291ad4add670fdd6e80e6c6efa
Andrei Zvonimir Crnković <andreicek@0x7f.dev>
2026-07-07 12:56:46 +0200

    Render business_unit as separate PartyIdentification (UBL schema)

--- lib/ex_cius/invoice_template_xml.ex
+++ lib/ex_cius/invoice_template_xml.ex
@@ -605,7 +605,8 @@ defmodule ExCius.InvoiceTemplateXML do
           "cac:Party",
           [
             build_endpoint_id(supplier.oib),
-            build_party_identification(supplier.oib, Map.get(supplier, :business_unit)),
+            build_party_identification(supplier.oib),
+            build_business_unit_identification(Map.get(supplier, :business_unit)),
             build_postal_address(supplier.postal_address),
             build_party_tax_scheme(supplier.party_tax_scheme),
             build_party_legal_entity(supplier.registration_name),
@@ -627,7 +628,8 @@ defmodule ExCius.InvoiceTemplateXML do
         "cac:Party",
         [
           build_endpoint_id(customer.oib),
-          build_party_identification(customer.oib, Map.get(customer, :business_unit)),
+          build_party_identification(customer.oib),
+          build_business_unit_identification(Map.get(customer, :business_unit)),
           build_postal_address(customer.postal_address),
           build_party_tax_scheme(customer.party_tax_scheme),
           build_party_legal_entity(customer.registration_name),
@@ -642,21 +644,18 @@ defmodule ExCius.InvoiceTemplateXML do
     element("cbc:EndpointID", [schemeID: "9934"], oib)
   end
 
-  defp build_party_identification(oib, business_unit) do
-    element(
-      "cac:PartyIdentification",
-      [
-        element("cbc:ID", "9934:#{oib}"),
-        build_business_unit_id(business_unit)
-      ]
-      |> Enum.reject(&is_nil/1)
-    )
+  defp build_party_identification(oib) do
+    element("cac:PartyIdentification", [
+      element("cbc:ID", "9934:#{oib}")
+    ])
   end
 
-  defp build_business_unit_id(nil), do: nil
+  defp build_business_unit_identification(nil), do: nil
 
-  defp build_business_unit_id(business_unit) do
-    element("cbc:ID", [schemeID: "HR99"], business_unit)
+  defp build_business_unit_identification(business_unit) do
+    element("cac:PartyIdentification", [
+      element("cbc:ID", [schemeID: "HR99"], business_unit)
+    ])
   end
 
   defp build_postal_address(address) do
